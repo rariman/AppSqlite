173e45e398c0241846d6b430f6b06adb85a4cb46
RARIMAN <rariman@gmail.com>
2015-05-30 14:35:38 -0300

    Firts commit

--- app/src/main/java/com/rariman/appsqlite/MainActivity.java
+++ app/src/main/java/com/rariman/appsqlite/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
-    @Override
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
