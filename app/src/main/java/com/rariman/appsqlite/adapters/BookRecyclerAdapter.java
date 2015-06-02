166e51422d4aa11c29a5c6bcca80d75df51a7f64
RARIMAN <rariman@gmail.com>
2015-06-01 23:05:10 -0300

    New files: - BookRecyclerAdapter.java to extend RecyclerView.Adapter.
    Changes:
    - BookListFragment.java : Handle a reference to recycleview,connected it to a LinearLayoutManager, and attach a BookRecyclerAdapter adapter to be displayed.
    - book_short_description.xml : changed android:id="@+id/tileTextView" to android:id="@+id/titleTextView".

---
+++ app/src/main/java/com/rariman/appsqlite/adapters/BookRecyclerAdapter.java
@@ -0,0 +1,56 @@
+package com.rariman.appsqlite.adapters;
+
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.rariman.appsqlite.R;
+import com.rariman.appsqlite.domain.Book;
+
+import java.util.List;
+
+/**
+ * Created by Rariman on 01/06/2015.
+ */
+public class BookRecyclerAdapter extends RecyclerView.Adapter<BookRecyclerAdapter.BookViewHolder> {
+
+    List<Book> bookList;
+
+    public BookRecyclerAdapter(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+
+    @Override
+    public BookViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Log.d("LOG", "onCreateViewHolder");
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.book_short_description, parent, false);
+        BookViewHolder bookViewHolder = new BookViewHolder(view);
+        return bookViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(BookViewHolder holder, int position) {
+        Log.d("LOG", "onBindViewHolder");
+        holder.title.setText(bookList.get(position).getTitle());
+        holder.description.setText(bookList.get(position).getDescription());
+    }
+
+    @Override
+    public int getItemCount() {
+        return bookList.size();
+    }
+
+    public class BookViewHolder extends RecyclerView.ViewHolder {
+        private TextView title;
+        private TextView description;
+
+        public BookViewHolder(View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.titleTextView);
+            description = (TextView) itemView.findViewById(R.id.descriptionTextView);
+        }
+    }
+}
--- app/src/main/java/com/rariman/appsqlite/fragments/BookListFragment.java
+++ app/src/main/java/com/rariman/appsqlite/fragments/BookListFragment.java
@@ -6,12 +6,19 @@ import android.graphics.Outline;
 import android.os.Build;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewOutlineProvider;
 
 import com.rariman.appsqlite.R;
+import com.rariman.appsqlite.adapters.BookRecyclerAdapter;
+import com.rariman.appsqlite.domain.Book;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -19,20 +26,44 @@ import com.rariman.appsqlite.R;
  */
 public class BookListFragment extends Fragment {
 
-    View addButton;
+    private View addButton;
+    private RecyclerView bookRecyclerView;
+    private List<Book> books;
+
 
     public BookListFragment() {
         // Required empty public constructor
     }
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        books = new ArrayList<>();
+        books.add(new Book("Harry Potter", "Hogwarts"));
+        books.add(new Book("The Lord of the Rings", "Frodo"));
+
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        RecyclerView.LayoutManager layoutManager;
+
         View view = inflater.inflate(R.layout.fragment_book_list, container, false);
 
         addButton = view.findViewById(R.id.add_button);
         addButton.setOutlineProvider(addButtonOutlineProvider);
         addButton.setClipToOutline(true);
+
+        bookRecyclerView = (RecyclerView)view.findViewById(R.id.bookRecyclerView);
+        bookRecyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(getActivity());
+        bookRecyclerView.setLayoutManager(layoutManager);
+        BookRecyclerAdapter bookRecyclerAdapter = new BookRecyclerAdapter(books);
+        bookRecyclerView.setAdapter(bookRecyclerAdapter);
+
+
         // Inflate the layout for this fragment
         return view;
     }
